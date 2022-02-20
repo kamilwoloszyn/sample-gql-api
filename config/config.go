865ad264149ed6f3bbf4c4e6c728ee8c14770a0e
Kamil Woloszyn <kamil.woloszyn1@gmail.com>
2022-02-20 09:54:40 +0100

    DatabaseName field

--- config/config.go
+++ config/config.go
@@ -8,4 +8,5 @@ type Config struct {
 	DatabaseUserName          string        `env:"DATABASE_USER_NAME" envDefault:"root"`
 	DatabasePassword          string        `env:"DATABASE_PASSWORD" envDefault:"pAsswordFORroot"`
 	DatabaseConnectionTimeout time.Duration `env:"DATABASE_CONNECTION_TIMEOUT" envDefault:"10s"`
+	DatabaseName              string        `env:"DATABASE_NAME" envDefault:"sampledb"`
 }
